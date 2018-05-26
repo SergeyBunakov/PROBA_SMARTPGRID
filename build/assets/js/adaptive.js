cebf5e2b694e70dc5eda7dbfb6e2bdc5270d845e
SergeyBunakov <bunakov.sergey@gmail.com>
2018-05-26 10:58:06 +0300

    Initial commit

---
+++ build/assets/js/adaptive.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.menu_JS').on('click', function(){
+
+        $('.menu').slideToggle(300, function () {
+
+            if($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+        });
+
+    });
+});
